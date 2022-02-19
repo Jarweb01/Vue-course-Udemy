43ebceac84eb217cdf138036caa5935654c1904b
Jarweb01 <Jarvisweb@mail.ru>
2022-02-19 19:00:14 +0300

    Update section-2.27

--- section-2.27/app.js
+++ section-2.27/app.js
@@ -2,14 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname: ''
     };
   },
   computed: {
     fullname() {
       console.log('fullname')
-      if (this.name === '') return '';
-      return this.name + '!'
+      // if (this.name === '') return '';
+      return this.name + ' ' + this.lastName;
     }
   },
   methods: {
@@ -31,6 +33,30 @@ const app = Vue.createApp({
     //   if (this.name === '') return '';
     //   return this.name + '!'
     // }
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function(){
+          that.counter = 0;
+        }, 2000)
+      }
+    }
+  //   name(value) {
+  //     if (value === '') {
+  //       this.fullname = '';
+  //     } else {
+  //       this.fullname = value + ' ' + this.lastName;
+  //     }
+  //   },
+  //   lastName(value) {
+  //     if (value === '') {
+  //       this.fullname = '';
+  //     } else {
+  //       this.fullname = this.name + ' ' + value;
+  //     }
+  //   }
   }
 });
 
