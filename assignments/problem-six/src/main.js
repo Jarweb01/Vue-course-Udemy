2c7cb48e847ea7de5a164fb22fc35eff51b3b288
Jarweb01 <jarvisweb@mail.ru>
2022-03-09 21:21:42 +0300

    Update problem-six

--- assignments/problem-six/src/main.js
+++ assignments/problem-six/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue';
 
 import ActiveUser from './components/ActiveUser.vue';
 import UserData from './components/UserData.vue';
 
+import App from './App.vue';
 
 const app = createApp(App);
 
