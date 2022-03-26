15657b87f855d3e1c5c6de746699220a8ff3a037
Jarweb01 <jarvisweb@mail.ru>
2022-03-26 13:24:11 +0300

    Finish section-15.223

--- section-15.212/src/main.js
+++ section-15.212/src/main.js
@@ -3,23 +3,21 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
+const counterModule = {
+  namespaced: true,
   state() {
     return {
       counter: 0,
-      isLoggedIn: false,
-    };
+    }
   },
   mutations: {
     increment(state) {
       state.counter = state.counter + 2;
     },
     increase(state, payload) {
+      console.log(state);
       state.counter = state.counter + payload.value;
     },
-    setAuth(state, payload) {
-      state.isLoggedIn = payload.isAuth;
-    }
   },
   actions: {
     increment(context) {
@@ -28,17 +26,17 @@ const store = createStore({
       }, 2000);
     },
     increase(context, payload) {
-      console.log(context)
+      console.log(context);
       context.commit('increase', payload);
     },
-    login(context) {
-      context.commit('setAuth', {isAuth: true});
-    },
-    logout(context) {
-      context.commit('setAuth', {isAuth: false});
-    },
+    login() {
+       //
+    }
   },
   getters: {
+    testAuth(state) {
+      return state.isLoggedIn;
+    },
     finalCounter(state) {
       return state.counter * 3;
     },
@@ -51,7 +49,33 @@ const store = createStore({
         return 100;
       }
       return finalCounter;
+    }
+  }
+};
+
+const store = createStore({
+  modules: {
+    numbers: counterModule,
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    setAuth(state, payload) {
+      state.isLoggedIn = payload.isAuth;
+    }
+  },
+  actions: {
+    login(context) {
+      context.commit('setAuth', {isAuth: true});
     },
+    logout(context) {
+      context.commit('setAuth', {isAuth: false});
+    },
+  },
+  getters: {
     userIsAuthenticated(state) {
       return state.isLoggedIn;
     }
