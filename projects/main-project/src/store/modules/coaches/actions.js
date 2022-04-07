19674307896964686d56524a575caf2979aa16c8
Jarweb01 <jarvisweb@mail.ru>
2022-04-07 16:47:11 +0300

    Finish 252 lesson

--- projects/main-project/src/store/modules/coaches/actions.js
+++ projects/main-project/src/store/modules/coaches/actions.js
@@ -11,10 +11,13 @@ export default {
       areas: data.areas,
     };
 
-    const response = await fetch(`https://coaches-project-39ad2-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
-      method: 'PUT',
-      body: JSON.stringify(coachData),
-    });
+    const response = await fetch(
+      `https://coaches-project-39ad2-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
 
     // const responseData = await response.json();
 
@@ -22,11 +25,35 @@ export default {
       // error ...
     }
 
-
-
     context.commit('registerCoach', {
       ...coachData,
-      id: userId
-    })
-  }
+      id: userId,
+    });
+  },
+  async loadCoaches(context, payload) {
+    const response = await fetch(
+      `https://coaches-project-39ad2-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // error
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches)
+  },
 };
