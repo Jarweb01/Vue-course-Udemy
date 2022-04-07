391056ba6811ac4fc43eb6fa0ba828c703a15a07
Jarweb01 <jarvisweb@mail.ru>
2022-04-07 15:35:43 +0300

    Finish 251 lesson

--- projects/main-project/src/store/modules/coaches/actions.js
+++ projects/main-project/src/store/modules/coaches/actions.js
@@ -1,8 +1,9 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
       // id: new Date().toISOString(),
-      id: context.rootGetters.userId,
+      // id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -10,6 +11,22 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData)
+    const response = await fetch(`https://coaches-project-39ad2-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData),
+    });
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
+    }
+
+
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    })
   }
 };
