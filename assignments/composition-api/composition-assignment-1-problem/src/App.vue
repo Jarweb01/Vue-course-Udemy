c3a3a0bd5adb3dfa194d6d51422a88545af0c1f4
Jarweb01 <jarvisweb@mail.ru>
2022-08-22 14:42:38 +0300

    Finish assignment 1 about composition api

---
+++ assignments/composition-api/composition-assignment-1-problem/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 v-if="goalIsVisible">{{ myCourseGoal }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggleGoal">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script>
+// import { reactive } from 'vue';
+
+export default {
+  data() {
+    return {
+      myCourseGoal: 'To master Vue!!!',
+      goalIsVisible: true,
+    };
+  },
+  methods: {
+    toggleGoal() {
+      this.goalIsVisible = !this.goalIsVisible;
+    },
+  },
+  // setup() {
+  //   const myCourseGoal = reactive({
+  //     title: 'To master Vue!',
+  //     goalIsVisible: true,
+  //     toggleGoal: () => {
+  //       myCourseGoal.goalIsVisible = !myCourseGoal.goalIsVisible;
+  //     },
+  //   });
+
+  //   return { myCourseGoal };
+  // },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
