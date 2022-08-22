bcfab7878a3fdd011b5a82eff3c6616a8bbdd67e
Jarweb01 <jarvisweb@mail.ru>
2022-08-22 18:21:26 +0300

    Added watchers to composition API

--- section-19.285/composition-01-starting-setup/src/App.vue
+++ section-19.285/composition-01-starting-setup/src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ userName }}</h2>
+    <h3>{{ userAge }}</h3>
     <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+    </div>
   </section>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 export default {
   setup() {
     // const uName = ref('Maximilian');
-    // const uAge = ref(29);
-    const user = reactive({
-      name: 'Maximilian',
-      age: 29,
+    // const user = reactive({
+    //   name: 'Maximilian',
+    //   age: 29,
+    // });
+    const uAge = ref(29);
+    const firstName = ref('');
+    const lastName = ref('');
+
+    const uName = computed(() => {
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    watch([uAge, uName], (newValues, oldValues) => {
+      console.log('old age: ' + oldValues);
+      console.log('new age: ', newValues);
     });
 
     const setNewAge = () => {
-      user.age += 2;
+      uAge.value += 2;
     };
 
-    return { user: user, setAge: setNewAge };
+    return {
+      userAge: uAge,
+      setAge: setNewAge,
+      userName: uName,
+      firstName,
+      lastName,
+    };
   },
   // data() {
   //   return {
