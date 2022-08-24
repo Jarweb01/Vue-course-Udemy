dadb86d72162c2ec00a4d9659f3e8c1de07d1c5c
Jarweb01 <jarvisweb@mail.ru>
2022-08-24 16:05:15 +0300

    Props and composition API

--- section-19.285/composition-01-starting-setup/src/App.vue
+++ section-19.285/composition-01-starting-setup/src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <section class="container">
+    <UserData :firstName="firstName" :lastName="lastName" :age="userAge" />
     <h2>{{ userName }}</h2>
     <h3>{{ userAge }}</h3>
     <button @click="setAge">Change Age</button>
     <div>
       <input type="text" placeholder="First Name" v-model="firstName" />
-      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
     </div>
   </section>
 </template>
 
 <script>
 import { ref, computed, watch } from 'vue';
+import UserData from './components/UserData.vue';
 
 export default {
+  components: {
+    UserData,
+  },
   setup() {
     // const uName = ref('Maximilian');
     // const user = reactive({
@@ -23,6 +29,7 @@ export default {
     const uAge = ref(29);
     const firstName = ref('');
     const lastName = ref('');
+    const lastNameInput = ref(null);
 
     const uName = computed(() => {
       return firstName.value + ' ' + lastName.value;
@@ -37,12 +44,18 @@ export default {
       uAge.value += 2;
     };
 
+    const setLastName = () => {
+      lastName.value = lastNameInput.value.value;
+    };
+
     return {
       userAge: uAge,
       setAge: setNewAge,
       userName: uName,
       firstName,
       lastName,
+      lastNameInput,
+      setLastName,
     };
   },
   // data() {
---
+++ section-19.285/composition-01-starting-setup/src/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>{{ userName }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  props: {
+    firstName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    age: {
+      type: Number,
+      required: false,
+    },
+  },
+  setup(props) {
+    const userName = computed(() => props.firstName + ' ' + props.lastName);
+    return {
+      userName,
+    };
+  },
+  // computed: {
+  //   userName() {
+  //     return this.firstName + ' ' + this.lastName;
+  //   },
+  // },
+};
+</script>
